9b46603081644cd85a92a5f5a85b45b7c03572fd
BugHunter-X <askzenai@gmail.com>
2025-01-05 13:56:39 +0100

    Add bugSolution.rb file

---
+++ bugSolution.rb
@@ -0,0 +1,22 @@
+```ruby
+class MyClass
+  def initialize(value)
+    @value = value
+  end
+
+  def value
+    @value
+  end
+
+  def value=(new_value)
+    @value = new_value
+  end
+end
+
+my_object = MyClass.new(10)
+puts my_object.value # => 10
+
+#Now we can modify the value using the setter
+my_object.value = 20 
+puts my_object.value # => 20
+```
